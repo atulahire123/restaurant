f81b8ef9eb4072a042ef9348e0d8c90fc0bc7a69
Atul Ahire <atulahire9607@gmail.com>
2024-05-23 14:23:32 +0530

    commit

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,17 +1,23 @@
-import {Fragment} from 'react';
+// Header.js
+import { Fragment } from 'react';
 import HeaderCartButton from './HeaderCartButton';
 import mealsImage from '../../assets/meals.jpg';
+import MealsSummary from '../Meals/MealsSummary';
 import classes from './Header.module.css';
-const Header=props =>
-    {
-      return<Fragment>
+
+const Header = (props) => {
+  return (
+    <Fragment>
       <header className={classes.header}>
-      <h1>ReactMeals</h1>
-      <HeaderCartButton/>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton />
       </header>
       <div className={classes['main-image']}>
-      <img src={mealsImage} alt=' A table full of delicous food:'/>
+        <img src={mealsImage} alt='A table full of delicious food' />
       </div>
-      </Fragment>  
-    };
-    export default Header;
+      <MealsSummary />
+    </Fragment>
+  );
+};
+
+export default Header;
